6712c3dcc20587370d3763ad54f56cdb48fce458
Vasya <VasyaPain2004@gmail.com>
2026-09-22 23:40:02 +0900

    completed quest9

--- src/s21_string/s21_string.c
+++ src/s21_string/s21_string.c
@@ -35,3 +35,22 @@ char *s21_strcpy(char *dest, const char *src) {
 
     return dest;
 }
+
+char *s21_strcat(char *dest, const char *src) {
+    int i = 0;
+    int j = 0;
+
+    while (dest[i] != '\0') {
+        i++;
+    }
+
+    while (src[j] != '\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return dest;
+}
--- src/s21_string/s21_string.h
+++ src/s21_string/s21_string.h
@@ -4,5 +4,6 @@
 int s21_strlen(const char *str);
 int s21_strcmp(const char *str1, const char *str2);
 char *s21_strcpy(char *dest, const char *src);
+char *s21_strcat(char *dest, const char *src);
 
 #endif
