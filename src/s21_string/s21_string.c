a7c193caf1800d84af72da8b52e4ac9a8ae91d4f
Vasya <VasyaPain2004@gmail.com>
2026-09-22 23:38:01 +0900

    completed quest8

--- src/s21_string/s21_string.c
+++ src/s21_string/s21_string.c
@@ -22,3 +22,16 @@ int s21_strcmp(const char *str1, const char *str2) {
 
     return str1[i] - str2[i];
 }
+
+char *s21_strcpy(char *dest, const char *src) {
+    int i = 0;
+
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+
+    return dest;
+}
--- src/s21_string/s21_string.h
+++ src/s21_string/s21_string.h
@@ -3,5 +3,6 @@
 
 int s21_strlen(const char *str);
 int s21_strcmp(const char *str1, const char *str2);
+char *s21_strcpy(char *dest, const char *src);
 
 #endif
