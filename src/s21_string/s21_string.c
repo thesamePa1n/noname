05fc56b94d55557f51a4f0098e420631e5141971
Vasya <VasyaPain2004@gmail.com>
2026-09-22 23:41:25 +0900

    completed quest10

--- src/s21_string/s21_string.c
+++ src/s21_string/s21_string.c
@@ -54,3 +54,20 @@ char *s21_strcat(char *dest, const char *src) {
 
     return dest;
 }
+
+char *s21_strchr(const char *str, int c) {
+    int i = 0;
+
+    while (str[i] != '\0') {
+        if (str[i] == c) {
+            return (char *)&str[i];
+        }
+        i++;
+    }
+
+    if (c == '\0') {
+        return (char *)&str[i];
+    }
+
+    return 0;
+}
