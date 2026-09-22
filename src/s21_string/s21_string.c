5e68b83562f812a624cd7a06980b009e0169a557
Vasya <VasyaPain2004@gmail.com>
2026-09-22 23:42:48 +0900

    completed quest11

--- src/s21_string/s21_string.c
+++ src/s21_string/s21_string.c
@@ -71,3 +71,30 @@ char *s21_strchr(const char *str, int c) {
 
     return 0;
 }
+
+char *s21_strstr(const char *haystack, const char *needle) {
+    int i = 0;
+    int j;
+
+    if (needle[0] == '\0') {
+        return (char *)haystack;
+    }
+
+    while (haystack[i] != '\0') {
+        j = 0;
+
+        while (haystack[i + j] != '\0' &&
+               needle[j] != '\0' &&
+               haystack[i + j] == needle[j]) {
+            j++;
+        }
+
+        if (needle[j] == '\0') {
+            return (char *)&haystack[i];
+        }
+
+        i++;
+    }
+
+    return 0;
+}
--- src/s21_string/s21_string.h
+++ src/s21_string/s21_string.h
@@ -6,5 +6,6 @@ int s21_strcmp(const char *str1, const char *str2);
 char *s21_strcpy(char *dest, const char *src);
 char *s21_strcat(char *dest, const char *src);
 char *s21_strchr(const char *str, int c);
+char *s21_strstr(const char *haystack, const char *needle);
 
 #endif
